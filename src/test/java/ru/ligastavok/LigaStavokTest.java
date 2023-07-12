94753309624880b3fb5479439eacaebf02458155
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-12 14:34:01 +0300

    small fixes

--- src/test/java/ru/ligastavok/LigaStavokTest.java
+++ src/test/java/ru/ligastavok/LigaStavokTest.java
@@ -1,4 +1,31 @@
 package ru.ligastavok;
 
-public class LigaStavokTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byCssSelector;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class LigaStavokTest extends LigaStavokTestBase {
+    @Tag("remote")
+    @Test
+    void smokePaymentsTest() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу", () -> open(baseUrl));
+        step("Кликаем по кнопке Вход", () -> {
+            $("div.wrapper_y30x.none_ZwlT")
+                    .shouldBe(Condition.visible)
+                    .click();
+        });
+
+
+        sleep(5000);
+    }
 }
